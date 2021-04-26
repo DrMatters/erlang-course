9b9b356cb6e936bfc3f8ad5120e44b17b2366949
DrMatters <qseminq@gmail.com>
2021-04-26 13:28:35 +0300

    fix

--- task1/mobius.erl
+++ task1/mobius.erl
@@ -2,31 +2,31 @@
 -export([is_prime/1, prime_factors/1, twos_factors/1, is_square_multiple/1, find_square_multiples/2]).
 -import(math, [sqrt/1]).
 
-is_prime(N) -> is_prime_helper(sqrt(N), N, 2).
+is_prime(N) -> is_prime_tail(sqrt(N), N, 2).
 
-is_prime_helper(Threshold, _, Divisor) when Divisor > Threshold -> true;
-is_prime_helper(Threshold, N, 2) -> N rem 2 > 0 andalso is_prime_helper(Threshold, N, 3);
-is_prime_helper(Threshold, N, Divisor) ->
+is_prime_tail(Threshold, _, Divisor) when Divisor > Threshold -> true;
+is_prime_tail(Threshold, N, 2) -> N rem 2 > 0 andalso is_prime_tail(Threshold, N, 3);
+is_prime_tail(Threshold, N, Divisor) ->
   if N rem Divisor == 0 -> false;
-    true -> is_prime_helper(Threshold, N, Divisor + 2)
+    true -> is_prime_tail(Threshold, N, Divisor + 2)
   end.
 
 prime_factors(N) ->
   {M, Twos} = twos_factors(N),
-  prime_factors_helper(M, 3, Twos).
+  prime_factors_tail(M, 3, Twos).
 
-prime_factors_helper(N, Divisor, Factors) ->
+prime_factors_tail(N, Divisor, Factors) ->
   if N =< 1 -> Factors;
-    N rem Divisor == 0 -> prime_factors_helper(N div Divisor, Divisor, [Divisor | Factors]);
-    true -> prime_factors_helper(N, Divisor + 2, Factors)
+    N rem Divisor == 0 -> prime_factors_tail(N div Divisor, Divisor, [Divisor | Factors]);
+    true -> prime_factors_tail(N, Divisor + 2, Factors)
   end.
 
 twos_factors(N) ->
-  twos_factors_helper(N, []).
+  twos_factors_tail(N, []).
 
-twos_factors_helper(N, Factors) ->
+twos_factors_tail(N, Factors) ->
   if N =< 1 -> {N, Factors};
-    N rem 2 == 0 -> twos_factors_helper(N div 2, [2 | Factors]);
+    N rem 2 == 0 -> twos_factors_tail(N div 2, [2 | Factors]);
     true -> {N, Factors}
   end.
 
